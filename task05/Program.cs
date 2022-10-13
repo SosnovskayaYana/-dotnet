7502e32aa4c5485f800024841dbe9daa19ec796d
Yana <yan4ik999@yandex.ru>
2022-10-13 13:06:58 +0300

    исправили ошибку

--- task05/Program.cs
+++ task05/Program.cs
@@ -3,11 +3,12 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if (number > 0)
 {
-int count = -number;
-while (count <= number);
-{
-    Console.Write($"{count} ");
-    count ++;
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count ++;
+    }
 }
-}
+
 else Console.WriteLine("Введите положительное число ");
