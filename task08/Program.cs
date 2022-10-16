93a2118ef9111d5c2912d6021448a0e8a33d6a8f
Yana <yan4ik999@yandex.ru>
2022-10-16 16:10:34 +0300

    02

---
+++ task08/Program.cs
@@ -0,0 +1,27 @@
+﻿// программа, которая на вход принимает число (N), а 
+// на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Программа построит ряд четных чисел от 1 до N. ");
+Console.Write("Введите значение N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 0)
+{
+
+    int count = 1;
+
+        while (count < N)
+        {
+            if ((N % 2) == 0)
+            {
+                Console.Write($"{count} ");
+            }
+                count++;
+        }
+
+}
+else Console.WriteLine("Введите положительное число. ");
