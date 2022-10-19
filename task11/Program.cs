7fc15a1e57278df1bde33743082336090373a768
Yana <yan4ik999@yandex.ru>
2022-10-19 12:03:37 +0300

    Task 11

---
+++ task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// программa, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число отрезка 100-999 => {number} ");
+
+int DelDigit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int digit = firstDigit * 10 + secondDigit;
+    return digit;
+}
+
+int digitDel = DelDigit(number);
+Console.WriteLine($"Искомое число => {digitDel} ");
