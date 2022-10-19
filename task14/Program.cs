295c8009f467d6889f58717fe72cb6722a4828e0
Yana <yan4ik999@yandex.ru>
2022-10-19 13:59:42 +0300

    task 14

--- task14/Program.cs
+++ task14/Program.cs
@@ -14,3 +14,31 @@ Console.WriteLine("Да ");
 
 else
 Console.WriteLine("Нет ");
+
+
+/* 
+решенеие через метод
+
+bool DivNum(int digit, int num1, int num2)
+{
+int result1 = digit%num1;
+int result2 = digit%num2;
+if(result1==0 && result2==0) return true;
+return false;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int number1 = 7;
+int number2 = 23;
+
+bool result = DivNum(num, number1, number2);
+if (result)
+{
+Console.WriteLine("Да ");
+}
+else
+{
+Console.WriteLine("Нет ");
+} 
+                     */
