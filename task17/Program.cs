a2eab1956006955af1dd12dabc3b0e5396343b02
Yana <yan4ik999@yandex.ru>
2022-10-20 10:33:52 +0300

    task0 17

--- task17/Program.cs
+++ task17/Program.cs
@@ -12,16 +12,16 @@ int y = Convert.ToInt32(Console.ReadLine());
 
 int Quarter(int xc, int yc)
 {
-    if(xc > 0 && yc > 0) return 1;
-    if(xc < 0 && yc > 0) return 2;
-    if(xc < 0 && yc < 0) return 3;
-    if(xc > 0 && yc < 0) return 4;
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
     return 0;
 }
 
 int quarter = Quarter(x, y);
-string result = quarter > 0 
-    ? $"Указанные координаты соответствуют четверти => {quarter}" 
+string result = quarter > 0
+    ? $"Указанные координаты соответствуют четверти => {quarter}"
     : "Введены некорректные координаты";
 
 Console.WriteLine(result);
