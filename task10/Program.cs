dcaed8de0783be3d2e5b73be118df77ff915e606
Yana <yan4ik999@yandex.ru>
2022-10-20 14:22:25 +0300

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// программa, принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int SecondDigit(int num)
+{
+    num = num / 10;
+    num = num % 10;
+    return num;
+}
+
+int secondDigit = SecondDigit(number);
+
+
+Console.WriteLine($"Вторая цифра {number} => {secondDigit}");
