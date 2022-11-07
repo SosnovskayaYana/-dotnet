2c928a80262b62eb078ca90c8ff2a8312bc42f50
Yana <yan4ik999@yandex.ru>
2022-11-07 12:21:36 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,31 @@
+﻿// программa, принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+
+int ProductOfNumbers(int numProduct)
+{
+    int multNum = 1;
+    for (int i = 1; i <= numProduct; i++)
+    {
+        multNum = multNum * i;
+    }
+    return multNum;
+}
+
+
+Console.WriteLine("Введите число: ");
+int numNr = Convert.ToInt32(Console.ReadLine());
+if (numNr > 0)
+{
+    int productOfNumbers = ProductOfNumbers(numNr);
+    Console.WriteLine($"Произведение чисел от 1 до {numNr} = {productOfNumbers}");
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
+
+
