ed78ab5c15593d4be2602131f0f7e620e9ace717
Yana <yan4ik999@yandex.ru>
2022-11-07 11:00:12 +0300

    task 24

---
+++ task24/Program.cs
@@ -0,0 +1,27 @@
+﻿// программa, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+int SumNumbers(int number)
+{
+    int sum = default;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i; // sum += 1;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("ВВедите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber = SumNumbers(num);
+
+Console.WriteLine(sumNumber);
