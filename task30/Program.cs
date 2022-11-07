6d85e038f28e3257e49d07428782e6aac1626360
Yana <yan4ik999@yandex.ru>
2022-11-07 12:57:40 +0300

    task30

---
+++ task30/Program.cs
@@ -0,0 +1,44 @@
+﻿// программa, выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+
+/* int[] array = new int [8];
+
+for (int i = 0; i < array.Length; i++)
+{
+ int random  = new Random().Next(0, 2);
+ Console.Write($"{random} ");
+}
+*/
+
+
+
+Console.WriteLine("Введите количество элементов массива : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] NameArray(int size)
+{
+    int[] array = new int[size]; //
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i<array.Length-1) Console.Write(",");
+    }
+    Console.Write("]");
+}
+
+int[] nameArray = NameArray(num);
+PrintArray(nameArray);
