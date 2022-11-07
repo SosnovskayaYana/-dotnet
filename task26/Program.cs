4e2e819ef26f60dc326fa803bff49b1bd9eb8bf4
Yana <yan4ik999@yandex.ru>
2022-11-07 11:57:54 +0300

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// программa, принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int SumDigit(int numDigit)
+{
+    numDigit = Math.Abs(numDigit);
+    int count = 0;
+    while (numDigit > 0)
+    {
+        numDigit = numDigit / 10;
+        count++;
+    }
+    return count > 0 ? count : 1;
+}
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int sumDigit = SumDigit(num);
+Console.WriteLine($"Количество цифр в числе = {sumDigit}");
