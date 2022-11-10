0ab182b738055583921f808ab02285866f888038
Yana <yan4ik999@yandex.ru>
2022-11-10 11:08:31 +0300

    task30

--- task30/Program.cs
+++ task30/Program.cs
@@ -20,7 +20,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 int[] NameArray(int size)
 {
-    int[] array = new int[size]; //
+    int[] array = new int[size]; 
     Random rnd = new Random();
     for (int i = 0; i < array.Length; i++)
     {
