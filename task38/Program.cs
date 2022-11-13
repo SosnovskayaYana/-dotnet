e11338df19b37d3deb5a571ebe5a5f47a182f1e5
Yana <yan4ik999@yandex.ru>
2022-11-13 14:55:21 +0300

    task38

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,19 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементом массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
 
-double[] CreateArrayRndDouble(int size, int min, int max)
-{
-    double[] array = new double[size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = rnd.NextDouble() * (max - min) + min;
-    }
-    return array;
-}
 
-
+//        метод создает массив со случайными вещественными числами
 double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] array = new double[size];
@@ -25,3 +17,57 @@ double[] CreateArrayRndDouble(int size, int min, int max)
     }
     return array;
 }
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double MaxElemArray(double[] arrayMaxElem)  // метод поиска мин элемента
+{
+    double max = arrayMaxElem[0];
+
+    for (int i = 0; i < arrayMaxElem.Length; i++)
+    {
+        if (arrayMaxElem[i] > max) max = arrayMaxElem[i];
+
+    }
+    return max;
+}
+
+double MinElemArray(double[] arrayMinElem)  // метод поиска мах элемента
+{
+    double min = arrayMinElem[0];
+
+    for (int i = 0; i < arrayMinElem.Length; i++)
+    {
+        if (arrayMinElem[i] < min) min = arrayMinElem[i];
+
+    }
+    return min;
+}
+
+double DiffMaxMinElem(double mx, double mn)
+{
+    double diff = mx - mn;
+    return diff;
+}
+
+
+double[] array = CreateArrayRndDouble(5, 1, 20);
+PrintArray(array);
+
+double maxElemArray = MaxElemArray(array);
+Console.WriteLine($"max = {maxElemArray}");
+
+double minElemArray = MinElemArray(array);
+Console.WriteLine($"min = {minElemArray}");
+
+double diffMaxMinElem = DiffMaxMinElem(maxElemArray, minElemArray);
+Console.WriteLine($"Разница между {maxElemArray} и {minElemArray} = {diffMaxMinElem}");
