a71551899bc88f55b286cbf7e2d669638f844dcf
Yana <yan4ik999@yandex.ru>
2022-11-17 10:23:35 +0300

    task40

---
+++ task40/Program.cs
@@ -0,0 +1,26 @@
+﻿/* программa, принимает на вход три
+числа и проверяет, может ли существовать треугольник с сторонами
+такой длины.
+
+Теорема о неравенстве треугольника: 
+каждая сторона треугольника меньше суммы двух других сторон. */
+
+
+Console.WriteLine("Введите три числа через Enter: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+bool IsExsistTriangle(int number1, int number2, int number3)
+{
+    if (number1 < number2 + number3 && number2 < number1 + number3 && number3 < number1 + number2)
+    {
+        return true;
+        // return number1 < number2 + number3 && number2 < number1 + number3 && number3 < number1 + number2;
+    }
+    else return false;
+}
+
+bool isExsistTriangle = IsExsistTriangle(num1, num2, num3);
+Console.WriteLine(isExsistTriangle);
