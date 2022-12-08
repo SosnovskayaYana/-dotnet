0a0825601db0974d9d39d5dfbf1d61664ebffd60
Yana <yan4ik999@yandex.ru>
2022-12-08 13:36:07 +0300

    task49 1

--- task49/Program.cs
+++ task49/Program.cs
@@ -59,3 +59,35 @@ Console.WriteLine();
 int[,] replaceEvenElemMatrix = ReplaceEvenElem(array2D);
 PrintMatrix(replaceEvenElemMatrix);
 
+
+
+/* Решение без методов
+
+int[,] matrix = new int[3, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i % 2 == 0 && j % 2 == 0)
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+*/
