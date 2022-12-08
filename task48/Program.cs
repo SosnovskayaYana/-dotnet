1d000c6f7bd2964ed7dc1fced11007183f4b3a03
Yana <yan4ik999@yandex.ru>
2022-12-08 12:52:24 +0300

    task 48 литер

--- task48/Program.cs
+++ task48/Program.cs
@@ -32,3 +32,27 @@ void PrintMatrix(int[,] array)
 int[,] mtr = CreateMatrix(3, 5);
 PrintMatrix(mtr);
 
+
+
+
+/* Решение без методов
+
+Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = i + j;
+        Console.Write(matrix[i, j] + "\t");  // литерал отступ
+    }
+    Console.WriteLine();
+}
+*/
