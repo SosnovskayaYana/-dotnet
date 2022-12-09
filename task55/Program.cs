48d6a0d8c64f375cb13f5a6f0c95efff7b4fe5a4
Yana <yan4ik999@yandex.ru>
2022-12-09 11:33:18 +0300

    task55 1

--- task55/Program.cs
+++ task55/Program.cs
@@ -3,6 +3,8 @@
 невозможно, программа должна вывести сообщение для
 пользователя. */
 
+/*
+
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
     int[,] matrix = new int[rows, columns];
@@ -62,7 +64,7 @@ return matrix;
 }
 */
 
-
+/*
 int[,]newMatrix = CreateMatrixRndInt(4, 4, 0, 10);
 PrintMatrix(newMatrix);
 Console.WriteLine("");
@@ -71,6 +73,81 @@ if(newMatrix.GetLength(0) == newMatrix.GetLength(1))
 {
 int[,] repleaceRowsToColumns = RepleaceRowsToColumns(newMatrix);
 PrintMatrix(repleaceRowsToColumns);
+}
+
+else Console.WriteLine("Невозможно заменить строки на столбцы ");  
+*/
+
+
+// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] GetArray(int m, int n, int min, int max)  // (int m, int n, int minValue, int maxValue)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < m; i++)  // m - кол-во строк
+    {
+        for (int j = 0; j < n; j++)  // n - кол-во столбцов
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);  // matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+/// печатает матрицу
+void PrintMatrix(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + "\t");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int[,] ReplaceRowsColumns(int[,] array)
+{
+    int[,] replaceArray = new int[array.GetLength(0), array.GetLength(1)];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                replaceArray[i, j] = array[j, i];
+            }
+        }
+
+    return replaceArray;
+}
+
+
+
+int[,] matrixArray = GetArray(rows, columns, 0, 10);
+PrintMatrix(matrixArray);
+
+Console.WriteLine();
+Console.WriteLine("Результат: ");
+
+if (rows == columns)
+{
+int[,] replace = ReplaceRowsColumns(matrixArray);
+
+Console.WriteLine();
+
+PrintMatrix(replace);
 }
 
 else Console.WriteLine("Невозможно заменить строки на столбцы ");
