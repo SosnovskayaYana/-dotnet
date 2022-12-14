3f6b8fde2c76df3cc120675a266136448d782996
Yana <yan4ik999@yandex.ru>
2022-12-14 13:38:15 +0300

    task 67 рекурсия

---
+++ task67/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumElem(int num)
+{
+if (num == 0) return num;
+return num % 10 + SumElem(num / 10);
+}
+
+
+int sumElem = SumElem(number);
+Console.Write($"Сумма цифр = {sumElem}");
