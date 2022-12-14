b174c8335cffbb8191038019350b5aa2d800ac85
Yana <yan4ik999@yandex.ru>
2022-12-14 20:28:32 +0300

    task 66 рекурсия

---
+++ task66/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задайте значения M и N. Напишите программу, которая 
+найдёт сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30  */
+
+int SumNatureNumbers(int numberA, int numberB)
+{
+    // int sum = 0;
+    if (numberA > numberB) return 0;
+    return SumNatureNumbers(numberA, numberB - 1) + numberB;
+}
+
+Console.Write("Введите первое натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (m < n)
+{
+    int result = SumNatureNumbers(m, n);
+    Console.Write($"Сумма натуральных элементов в промежутке от {m} до {n}  = {result}");
+}
+
+if (m > n)
+{
+    int result = SumNatureNumbers(n, m);
+    Console.Write($"Сумма натуральных элементов в промежутке от {n} до {m}  = {result}");
+}
+
+if (m == n)
+{
+    Console.Write($"Некорректный ввод");
+}
